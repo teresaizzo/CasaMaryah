38102ea295735a607334895ed64e6c2f5d93b513
benedettosimone <sim.benede@gmail.com>
2022-09-15 12:06:04 +0200

    footer + contact section responsive <600 + color

--- assets/js/gallery.js
+++ assets/js/gallery.js
@@ -1,7 +1,7 @@
 let galleryImages = document.querySelectorAll(".gallery-img");
 let getLatestOpenedImg;
 let windowWidth = window.innerWidth;
-let totalImages = 15;
+let totalImages = 16;
 
 
 let showAllButton = document.querySelector("#more-images");
